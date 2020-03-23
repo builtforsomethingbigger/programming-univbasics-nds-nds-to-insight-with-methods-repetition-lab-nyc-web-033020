24969fe5e4ed0ce266605aa539ab3d54f948cc3e
Stephen Cases <builtforsomethingbigger@gmail.com>
2020-03-23 16:15:42 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -49,6 +49,6 @@ def total_gross(source)
   # returned by directors_totals, and add it to a running total. When done,
   # return the total
   directors_totals = directors_totals(source)
-  list_of_directors = Array.new
+  list_of_directors = list_of_directors(source)
 
 end
