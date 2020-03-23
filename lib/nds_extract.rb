3018264a0c9467240e0ed906e8bb0f46cc224d86
Stephen Cases <builtforsomethingbigger@gmail.com>
2020-03-23 16:05:35 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -1,5 +1,8 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 require_relative './directors_database'
+requre 'pp'
+
+pp directors_database
 
 def directors_totals(source)
   result = {}
@@ -39,5 +42,3 @@ def total_gross(source)
   # returned by directors_totals, and add it to a running total. When done,
   # return the total
 end
-
-
